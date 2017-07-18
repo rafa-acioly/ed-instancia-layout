c1f43c8055fa1f5e67402a9efb4d3d01df6d5de7
Rafael <aciolyr@gmail.com>
2017-07-18 15:17:52 -0300

    efeito de click em tabs finalizado

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,49 @@
 import $ from 'jquery';
+var iconBase = require('../images/pin-map.png');
 
 $(document).ready(function () {
-  console.log('oi');
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: new google.maps.LatLng(-23.526776, -46.339726),
+    mapTypeId: 'terrain'
+  });
+
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(-23.526776, -46.339726),
+    icon: iconBase,
+    map: map
+  });
+
+  $('.tabs li').on('click', function (event) {
+    event.preventDefault();
+    // tabs to toggle "is-active" class.
+    const tabs = $('.tabs ul li');
+
+    // current tab clicked
+    const it = $(this);
+
+    // current data attribute
+    const thisdata = it.data('tab');
+
+    // get all contents
+    const contents = $('.tabs-content p[data-content]');
+
+    /**
+     * Loop over all contents, if it is not the 
+     * same data attribute then hide.
+     */
+    contents.each(function(index, element) {
+      if ($(element).data('content') !== thisdata) {
+        element.classList.add('is-hidden');
+      } else {
+        element.classList.remove('is-hidden');
+      }
+    });
+
+    // Toggle class from another tabs that is not the clicked one.
+    tabs.not(it).removeClass('is-active');
+
+    // add active class to clicked
+    it.addClass('is-active');
+  });
 });
