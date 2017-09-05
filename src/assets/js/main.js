2a404ca5588d2ec525cd737dd54d57ec6dea37a4
Rafael <aciolyr@gmail.com>
2017-09-05 14:01:04 -0300

    mapa e imagens de planta alterados layout

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -14,33 +14,6 @@ $(document).ready(function () {
     map: map
   });
 
-  $('.tabs li').on('click', function (event) {
-    event.preventDefault();
-    // tabs to toggle "is-active" class.
-    const tabs = $('.tabs ul li');
-    // current tab clicked
-    const it = $(this);
-    // current data attribute
-    const thisdata = it.data('tab');
-    // get all contents
-    const contents = $('.tabs-content p[data-content]');
-    /**
-     * Loop over all contents, if it is not the 
-     * same data attribute then hide.
-     */
-    contents.each(function(index, element) {
-      if ($(element).data('content') !== thisdata) {
-        element.classList.add('is-hidden');
-      } else {
-        element.classList.remove('is-hidden');
-      }
-    });
-    // Toggle class from another tabs that is not the clicked one.
-    tabs.not(it).removeClass('is-active');
-    // add active class to clicked
-    it.addClass('is-active');
-  });
-
   $('a[href="#contact"').on('click', function (event) {
     event.preventDefault();
 
